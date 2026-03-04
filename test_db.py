f79c6d0c9cf4a875071e6da28dea27b35d1eb082
Kieran Kharin <kieran.kharin@gmail.com>
2026-03-04 16:16:45 +0100

    added .env and test_db

---
+++ test_db.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+import mysql.connector
+
+load_dotenv()
+
+conn = mysql.connector.connect(
+    host=os.getenv('DB_HOST'),
+    port=int(os.getenv('DB_PORT', 3306)),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+)
+
+cursor = conn.cursor()
+cursor.execute("SELECT VERSION()")
+print("MySQL version: " + cursor.fetchone()[0])
+cursor.close()
+print("Connected: ", conn.is_connected())
+conn.close()
